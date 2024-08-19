a679279f5aa45d62612d7b5b474421f24ba16be9
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2024-08-19 20:14:45 +0530

    feat: added register page

--- client/src/app/(auth)/register/page.tsx
+++ client/src/app/(auth)/register/page.tsx
@@ -1,5 +1,24 @@
+import Register from "@/components/auth/Register";
+import Link from "next/link";
 import React from "react";
 
 export default function RegisterPage() {
-  return <div></div>;
+  return (
+    <div className="flex justify-center items-center h-screen ">
+      <div className="w-full px-10 md:w-[550px] shadow-md rounded-xl py-5 bg-white">
+        <div>
+          <h1 className="text-4xl text-center font-extrabold bg-gradient-to-r from-pink-400 to-purple-500 text-transparent bg-clip-text">
+            Clash <span className="text-black font-bold">Register</span>
+          </h1>
+        </div>
+        <Register />
+        <p className="text-center mt-3">
+          Already have an account ?{" "}
+          <strong>
+            <Link href="/login">Login</Link>
+          </strong>
+        </p>
+      </div>
+    </div>
+  );
 }
--- client/src/components/auth/Register.tsx
+++ client/src/components/auth/Register.tsx
@@ -3,12 +3,16 @@ import React from "react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { SubmitButton } from "../common/SubmitBtn";
-import Link from "next/link";
 import { Button } from "../ui/button";
 
-export default function Login() {
+export default function Register() {
   return (
     <form>
+      <div className="mt-4">
+        <Label htmlFor="name">Name</Label>
+        <Input placeholder="Type your name" name="name" />
+        {/* <span className="text-red-400">{state.errors?.name}</span> */}
+      </div>
       <div className="mt-4">
         <Label htmlFor="email">Email</Label>
         <Input placeholder="Type your email" name="email" />
@@ -21,13 +25,19 @@ export default function Login() {
           placeholder="Type your password"
           name="password"
         />
-        <div className="text-right underline mt-2">
-          <Link href="/forgot-password">Forgot Password?</Link>
-        </div>
         {/* <span className="text-red-400">{state.errors?.password}</span> */}
       </div>
-      <div className="mt-5">
-        <Button className="w-full">Login</Button>
+      <div className="mt-4">
+        <Label htmlFor="cpassword">Confirm Password</Label>
+        <Input
+          type="password"
+          placeholder="Type your password"
+          name="confirm_password"
+        />
+        {/* <span className="text-red-400">{state.errors?.confirm_password}</span> */}
+      </div>
+      <div className="mt-6">
+        <Button className="w-full">Register</Button>
       </div>
     </form>
   );
